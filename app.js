723410619656502a9f261f7c2b2861b0d73a7fda
Jay Cortes <cortesjay1991@gmail.com>
2025-08-30 19:37:19 +0200

    Data Collection

--- app.js
+++ app.js
@@ -7,4 +7,15 @@ $(function() {
         // For a post with id: 5, Backbone knows to go to 'http://localhost:3000/posts/5'
         urlRoot: 'http://localhost:3000/posts'
     });
+
+     // --- THE DATA COLLECTION ---
+    // Represents a LIST of blog posts.
+    const Posts = Backbone.Collection.extend({
+        // Tells this Collection that it will be holding a list of 'Post' models.
+        model: Post,
+
+        // This tells the Collection where to get the ENTIRE LIST of posts.
+        // When we call .fetch(), it will make a GET request to this URL.
+        url: 'http://localhost:3000/posts'
+    });
 });
